80937cd563dccae437d7a5ea4026a608d787882b
Steve Skladany <steve@skladany.net>
2023-03-12 15:19:06 -0400

    bootstrap: render logo and title

--- src/index.exercise.js
+++ src/index.exercise.js
@@ -1,9 +1,33 @@
 // 🐨 you'll need to import react and createRoot from react-dom up here
+import React from 'react'
+import ReactDOM from 'react-dom'
 
 // 🐨 you'll also need to import the Logo component from './components/logo'
+import {Logo} from './components/logo'
 
 // 🐨 create an App component here and render the logo, the title ("Bookshelf"), a login button, and a register button.
 // 🐨 for fun, you can add event handlers for both buttons to alert that the button was clicked
 
 // 🐨 use createRoot to render the <App /> to the root element
 // 💰 find the root element with: document.getElementById('root')
+
+function App() {
+  const handleClick = e => {
+    alert(`${e.target.innerText} clicked`)
+  }
+
+  return (
+    <div>
+      <Logo width="80" height="80" />
+      <h1>Bookshelf</h1>
+      <div>
+        <button onClick={handleClick}>Login</button>
+      </div>
+      <div>
+        <button onClick={handleClick}>Register</button>
+      </div>
+    </div>
+  )
+}
+
+ReactDOM.render(<App />, document.getElementById('root'))
