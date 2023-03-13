9cbbf085b84b0c5e5806a862c423ac8832a9dfce
Steve Skladany <steve@skladany.net>
2023-03-13 19:22:12 -0400

    bootstrap: @reach/dialog - extra credit 01

--- src/index.exercise.js
+++ src/index.exercise.js
@@ -1,9 +1,11 @@
 // 🐨 you'll need to import react and createRoot from react-dom up here
+import '@reach/dialog/styles.css'
 import React from 'react'
 import ReactDOM from 'react-dom'
 
 // 🐨 you'll also need to import the Logo component from './components/logo'
 import {Logo} from './components/logo'
+import {Dialog} from '@reach/dialog'
 
 // 🐨 create an App component here and render the logo, the title ("Bookshelf"), a login button, and a register button.
 // 🐨 for fun, you can add event handlers for both buttons to alert that the button was clicked
@@ -12,20 +14,39 @@ import {Logo} from './components/logo'
 // 💰 find the root element with: document.getElementById('root')
 
 function App() {
-  const handleClick = e => {
-    alert(`${e.target.innerText} clicked`)
-  }
+  const [openModal, setOpenModal] = React.useState('none')
 
   return (
     <div>
       <Logo width="80" height="80" />
       <h1>Bookshelf</h1>
       <div>
-        <button onClick={handleClick}>Login</button>
+        <button onClick={() => setOpenModal('login')}>Login</button>
       </div>
       <div>
-        <button onClick={handleClick}>Register</button>
+        <button onClick={() => setOpenModal('register')}>Register</button>
       </div>
+      <Dialog
+        aria-label="Login form"
+        isOpen={openModal === 'login'}
+        onDismiss={() => setOpenModal('none')}
+      >
+        <button className="close-button" onClick={() => setOpenModal('none')}>
+          <span aria-hidden>Close</span>
+        </button>
+        <h3>Login</h3>
+      </Dialog>
+
+      <Dialog
+        aria-label="Registration form"
+        isOpen={openModal === 'register'}
+        onDismiss={() => setOpenModal('close')}
+      >
+        <button className="close-button" onClick={() => setOpenModal('none')}>
+          <span aria-hidden>Close</span>
+        </button>
+        <h3>Register</h3>
+      </Dialog>
     </div>
   )
 }
