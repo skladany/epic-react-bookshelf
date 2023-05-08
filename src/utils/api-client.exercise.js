837f0b3661ef3c45672d89513850b3a1a6dc4145
Steve Skladany <steve@skladany.net>
2023-05-08 18:29:59 -0400

    Authentication: Extra Credit 4, Support posting data

--- src/utils/api-client.exercise.js
+++ src/utils/api-client.exercise.js
@@ -3,26 +3,27 @@ const apiURL = process.env.REACT_APP_API_URL
 
 function client(
   endpoint,
-  {token, headers: customHeaders, ...customConfig} = {},
+  {token, headers: customHeaders, data, ...customConfig} = {},
 ) {
   const config = {
+    method: data ? 'POST' : 'GET',
+    body: data ? JSON.stringify(data) : undefined,
     headers: {
       Authorization: token ? `Bearer ${token}` : undefined,
+      'Content-type': data ? 'application/json' : undefined,
       ...customHeaders,
     },
     ...customConfig,
   }
 
   return window.fetch(`${apiURL}/${endpoint}`, config).then(async response => {
-    const data = await response.json()
-
-    // Log out user
     if (response.status === 401) {
       await auth.logout()
+      // refresh the page for them
       window.location.assign(window.location)
-      return Promise.reject({message: 'Please re-authenticate'})
+      return Promise.reject({message: 'Please re-authenticate.'})
     }
-
+    const data = await response.json()
     if (response.ok) {
       return data
     } else {
